636db9e32c378a7ed7133cd4e336f9f7eb1123ee
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-02-07 17:04:32 -0800

    add WriteMessages

--- src/Microsoft.AspNetCore.Sockets.Common/TextMessageBatchFormatter.cs
+++ src/Microsoft.AspNetCore.Sockets.Common/TextMessageBatchFormatter.cs
@@ -7,6 +7,12 @@ namespace Microsoft.AspNetCore.Sockets
 {
     public static class TextMessageBatchFormatter
     {
+        /// <summary>
+        /// Decodes the messages encoded in the provided data.
+        /// </summary>
+        /// <param name="data">The data containing the encoded messages to read</param>
+        /// <exception cref="FormatException">The input does not adhere to the protocol</exception>
+        /// <returns>A list of <see cref="Message"/> values decoded from the buffer</returns>
         public static IEnumerable<Message> ReadMessages(ReadOnlySpan<byte> data)
         {
             if (data.Length < 1 || data[0] != 'T')
@@ -31,6 +37,97 @@ namespace Microsoft.AspNetCore.Sockets
 
                 yield return new Message(payload, type, endOfMessage: true);
             }
+        }
+
+        /// <summary>
+        /// Attempts to write the specified messages to the target buffer
+        /// </summary>
+        /// <param name="target">The buffer in which to encode the messages</param>
+        /// <param name="messages">The messages to encode</param>
+        /// <returns>A boolean indicating if there was enough space in the buffer to encode the message.</returns>
+        public static bool WriteMessages(Span<byte> target, IReadOnlyList<Message> messages)
+        {
+            var size = MeasureMessages(messages);
+            if(target.Length < size)
+            {
+                return false;
+            }
+
+            int cursor = 0;
+            target[cursor] = (byte)'T';
+            cursor++;
+
+            foreach(var message in messages)
+            {
+                WriteMessage(target, message, ref cursor);
+            }
+            return true;
+        }
+
+        private static void WriteMessage(Span<byte> target, Message message, ref int cursor)
+        {
+            // Write the length as a UTF-8 string
+            var len = message.Payload.Buffer.Length.ToString();
+            var buf = Encoding.UTF8.GetBytes(len);
+            buf.CopyTo(target.Slice(cursor, buf.Length));
+            cursor += buf.Length;
+
+            // Separator
+            target[cursor] = (byte)':';
+            cursor++;
+
+            // Write the type character
+            target[cursor] = GetTypeCharacter(message.Type);
+            cursor++;
+
+            // Separator
+            target[cursor] = (byte)':';
+            cursor++;
+
+            // Payload
+            if(message.Type == MessageType.Binary)
+            {
+                var payload = Encoding.UTF8.GetBytes(Convert.ToBase64String(message.Payload.Buffer.ToArray()));
+                payload.CopyTo(target.Slice(cursor, payload.Length));
+                cursor += payload.Length;
+            } else
+            {
+                message.Payload.Buffer.CopyTo(target.Slice(cursor, message.Payload.Buffer.Length));
+                cursor += message.Payload.Buffer.Length;
+            }
+
+            // Terminator
+            target[cursor] = (byte)';';
+            cursor++;
+        }
+
+        public static int MeasureMessages(IReadOnlyList<Message> messages)
+        {
+            var size = 1; // 1 character for the 'T'
+            foreach(var message in messages)
+            {
+                size += MeasureMessage(message);
+            }
+            return size;
+        }
+
+        private static int MeasureMessage(Message message)
+        {
+            var size = 4; // two ':', one ';' and the type flag
+
+            // Add enough characters for the payload
+            var len = message.Type == MessageType.Binary ?
+                // Length is measured as Base-64 character length
+                (int)(4 * Math.Ceiling(((double)message.Payload.Buffer.Length / 3))) :
+                // Length is just number of bytes in the (already UTF-8 encoded) payload
+                message.Payload.Buffer.Length;
+            size += len;
+
+            // Add enough character to represent the length in digits
+            // Log10(0) == Infinity, so don't allow that to happen.
+            var digits = len == 0 ? 1 : (int)Math.Floor(Math.Log10(len)) + 1;
+            size += digits;
+            return size;
         }
 
         private static PreservedBuffer ParsePayload(ReadOnlySpan<byte> data, int length, MessageType messageType, ref int cursor)
@@ -118,12 +215,25 @@ namespace Microsoft.AspNetCore.Sockets
             return length;
         }
 
+        // There isn't a Span.IndexOf that takes a start point :(.
         private static int IndexOf(byte c, ReadOnlySpan<byte> data, int start)
         {
             // Scan to the end or to the matching character
             int cursor = start;
             for (; cursor < data.Length && data[cursor] != c; cursor++) ;
             return cursor;
+        }
+
+        private static byte GetTypeCharacter(MessageType type)
+        {
+            switch (type)
+            {
+                case MessageType.Text: return (byte)'T';
+                case MessageType.Binary: return (byte)'B';
+                case MessageType.Close: return (byte)'C';
+                case MessageType.Error: return (byte)'E';
+                default: throw new InvalidOperationException($"Unknown message type: {type}");
+            }
         }
     }
 }
--- test/Microsoft.AspNetCore.Sockets.Common.Tests/TextMessageBatchFormatterTests.cs
+++ test/Microsoft.AspNetCore.Sockets.Common.Tests/TextMessageBatchFormatterTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipelines;
 using System.Linq;
 using System.Text;
 using Xunit;
@@ -7,6 +9,24 @@ namespace Microsoft.AspNetCore.Sockets.Tests
 {
     public class TextMessageBatchFormatterTests
     {
+        public static IEnumerable<object[]> MeasureMessagesData
+        {
+            get
+            {
+                yield return new object[] { new[] { CreateMessage("abc") }, 9 };
+                yield return new object[] { new[] { CreateMessage("abc"), CreateMessage("cde", MessageType.Close) }, 17 };
+                yield return new object[] { new[] { CreateMessage(new byte[] { 0x0A, 0x0B }) }, 10 };
+                yield return new object[] { new[] { CreateMessage(new byte[] { 0x0A, 0x0B, 0x0C }) }, 10 }; // Same length, because of the magic of Base-64
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(MeasureMessagesData))]
+        public void MeasureMessagesTests(Message[] messages, int length)
+        {
+            Assert.Equal(length, TextMessageBatchFormatter.MeasureMessages(messages));
+        }
+
         [Theory]
         [InlineData("T0:T:;", MessageType.Text, "")]
         [InlineData("T3:T:ABC;", MessageType.Text, "ABC")]
@@ -15,9 +35,33 @@ namespace Microsoft.AspNetCore.Sockets.Tests
         [InlineData("T17:C:Connection Closed;", MessageType.Close, "Connection Closed")]
         [InlineData("T0:E:;", MessageType.Error, "")]
         [InlineData("T12:E:Server Error;", MessageType.Error, "Server Error")]
-        public void ReadSingleTextMessage(string data, MessageType messageType, string payload)
+        public void WriteSingleTextMessage(string encoded, MessageType messageType, string payload)
         {
-            var buffer = Encoding.UTF8.GetBytes(data);
+            var messages = new[] {
+                new Message(
+                    ReadableBuffer.Create(Encoding.UTF8.GetBytes(payload)).Preserve(),
+                    messageType,
+                    endOfMessage: true)
+            };
+            var encodedLength = TextMessageBatchFormatter.MeasureMessages(messages);
+            var buffer = new byte[encodedLength];
+
+            Assert.True(TextMessageBatchFormatter.WriteMessages(buffer, messages));
+
+            Assert.Equal(encoded, Encoding.UTF8.GetString(buffer));
+        }
+
+        [Theory]
+        [InlineData("T0:T:;", MessageType.Text, "")]
+        [InlineData("T3:T:ABC;", MessageType.Text, "ABC")]
+        [InlineData("T11:T:A\nR\rC\r\n;DEF;", MessageType.Text, "A\nR\rC\r\n;DEF")]
+        [InlineData("T0:C:;", MessageType.Close, "")]
+        [InlineData("T17:C:Connection Closed;", MessageType.Close, "Connection Closed")]
+        [InlineData("T0:E:;", MessageType.Error, "")]
+        [InlineData("T12:E:Server Error;", MessageType.Error, "Server Error")]
+        public void ReadSingleTextMessage(string encoded, MessageType messageType, string payload)
+        {
+            var buffer = Encoding.UTF8.GetBytes(encoded);
             var messages = TextMessageBatchFormatter.ReadMessages(buffer).ToArray();
 
             Assert.Equal(1, messages.Length);
@@ -27,9 +71,9 @@ namespace Microsoft.AspNetCore.Sockets.Tests
         [Theory]
         [InlineData("T0:B:;", new byte[0])]
         [InlineData("T8:B:q83vEg==;", new byte[] { 0xAB, 0xCD, 0xEF, 0x12 })]
-        public void ReadSingleBinaryMessage(string data, byte[] payload)
+        public void ReadSingleBinaryMessage(string encoded, byte[] payload)
         {
-            var buffer = Encoding.UTF8.GetBytes(data);
+            var buffer = Encoding.UTF8.GetBytes(encoded);
             var messages = TextMessageBatchFormatter.ReadMessages(buffer).ToArray();
 
             Assert.Equal(1, messages.Length);
@@ -39,8 +83,8 @@ namespace Microsoft.AspNetCore.Sockets.Tests
         [Fact]
         public void ReadMultipleMessages()
         {
-            const string data = "T0:B:;14:T:Hello,\r\nWorld!;1:C:A;12:E:Server Error;";
-            var buffer = Encoding.UTF8.GetBytes(data);
+            const string encoded = "T0:B:;14:T:Hello,\r\nWorld!;1:C:A;12:E:Server Error;";
+            var buffer = Encoding.UTF8.GetBytes(encoded);
             var messages = TextMessageBatchFormatter.ReadMessages(buffer).ToArray();
 
             Assert.Equal(4, messages.Length);
@@ -61,9 +105,9 @@ namespace Microsoft.AspNetCore.Sockets.Tests
         [InlineData("T5:T:A", "Unexpected end-of-message while reading Payload field.")]
         [InlineData("T5:T:ABCDE", "Unexpected end-of-message while reading Payload field.")]
         [InlineData("T5:T:ABCDEF", "Payload is missing trailer character ';'.")]
-        public void InvalidMessages(string data, string message)
+        public void InvalidMessages(string encoded, string message)
         {
-            var ex = Assert.Throws<FormatException>(() => TextMessageBatchFormatter.ReadMessages(Encoding.UTF8.GetBytes(data)).ToArray());
+            var ex = Assert.Throws<FormatException>(() => TextMessageBatchFormatter.ReadMessages(Encoding.UTF8.GetBytes(encoded)).ToArray());
             Assert.Equal(message, ex.Message);
         }
 
@@ -79,6 +123,22 @@ namespace Microsoft.AspNetCore.Sockets.Tests
             Assert.True(message.EndOfMessage);
             Assert.Equal(messageType, message.Type);
             Assert.Equal(payload, Encoding.UTF8.GetString(message.Payload.Buffer.ToArray()));
+        }
+
+        private static Message CreateMessage(byte[] payload, MessageType type = MessageType.Binary)
+        {
+            return new Message(
+                ReadableBuffer.Create(payload).Preserve(),
+                type,
+                endOfMessage: true);
+        }
+
+        private static Message CreateMessage(string payload, MessageType type = MessageType.Text)
+        {
+            return new Message(
+                ReadableBuffer.Create(Encoding.UTF8.GetBytes(payload)).Preserve(),
+                type,
+                endOfMessage: true);
         }
     }
 }
